59e47eb8f6a0024e4aa8edc22c286e37acc0d5e7
Nick <satur.1512@gmail.com>
2021-11-01 01:17:53 -0300

    changed order Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,17 @@ import { HomePageComponent } from '@modules/home/pages/home-page/home-page.compo
 
 
 const routes: Routes = [
+  //FIXME: curioso! el orden importa!
+  {
+    path:'auth', //TODO: localhost:4200/auth
+    loadChildren: () => import('./modules/auth/auth.module').then( m => m.AuthModule),
+  },
   {
     path:'', //TODO: localhost:4200/
     component: HomePageComponent, //llamamos al componente HomePage
     loadChildren: () => import('./modules/home/home.module').then( m => m.HomeModule) , //TODO: es una importacion dinamica de un módulo.
     //FIXME: los módulos que importemos tienen que tener rutas. 
-  },
-  {
-    path:'auth', //TODO: localhost:4200/
-    loadChildren: () => import('./modules/auth/auth.module').then( m => m.AuthModule),
-  },
+  }
 ];
 
 @NgModule({
