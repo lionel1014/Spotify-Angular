55527983245b630c7d0cc6cd2d5e05b1c5917ba6
Nick <satur.1512@gmail.com>
2021-11-14 22:26:46 -0300

    more tests

--- src/app/modules/auth/services/auth.service.spec.ts
+++ src/app/modules/auth/services/auth.service.spec.ts
@@ -2,20 +2,58 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import * as mockRaw from '../../../data/user.json'
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let mockUser : any = (mockRaw as any).default;
+  let httpClientSpy : { post : jasmine.Spy}
 
-  beforeEach(() => {
+  beforeEach(() => { //lo que se ejecuta entes de cada enunciado
     TestBed.configureTestingModule({
       imports:[
         HttpClientTestingModule
       ]
     });
-    service = TestBed.inject(AuthService);
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient' , ['post'])
+
+    //nuevo instanciamiento del servicio
+    service = new AuthService(httpClientSpy as any)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  //Prueba sendCredentials
+  it('Debe retornar un objeto con "data" y "tokensession"', (done:DoneFn) =>{
+
+    //Pruebas AAA -> Arrange (preparar)
+
+    const user : any = mockUser.userOk; //usuario a usar
+
+    const mockResponse = {
+      data: {},
+      tokenSession:'0x0x0x',
+    }
+
+    httpClientSpy.post.and.returnValue( of(mockResponse) ) //lo que la API va a responder
+
+    //Act(actuar)
+    service.sendCredentials(user.email, user. password )
+      .subscribe(responseAPI =>{
+
+        const getProperties = Object.keys(responseAPI) // ['data','tokenSession']
+
+        //Assert(afirmar)
+        expect(getProperties).toContain('data')
+        expect(getProperties).toContain('tokenSession')
+        console.log('Response API❤❤❤ ->',responseAPI);
+        done() //done porque es asincrona la paticion
+
+      })
+
+  })
 });
--- src/app/shared/pipe/order-list.pipe.spec.ts
+++ src/app/shared/pipe/order-list.pipe.spec.ts
@@ -1,8 +1,44 @@
 import { OrderListPipe } from './order-list.pipe';
+import * as mockRaw from '../../data/tracks.json'
+import { TrackModel } from '@core/models/tracks.model';
 
 describe('OrderListPipe', () => {
   it('create an instance', () => {
     const pipe = new OrderListPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('El array de entrada tiene que ser el mismo de salida',()=>{
+    
+    //Arrage arrancar
+    const pipe = new OrderListPipe();
+    const {data} : any = (mockRaw as any).default
+
+    //Act (accion)
+    const result : TrackModel[] = pipe.transform(data,'');
+
+    //Assert (afirmar)
+    expect(result).toBe(data);
+
+  })
+
+  it('El array tiene que estar en orden ASC',()=>{
+
+    //Arrage (arrancar)
+    const pipe = new OrderListPipe();
+    const {data} : any = (mockRaw as any).default;
+    const firstValue = data.find(( i:any ) => i._id === 7); //50cent
+    const lastValue = data.find(( i:any ) => i._id === 6); // TNT
+
+    //Act (accion)
+    const result : TrackModel[] = pipe.transform(data,'name','asc');
+    const firstResult = result[0];
+    const lasttResult = result[result.length - 1];
+    
+    //Assert (afirmar)
+    expect(firstResult).toEqual(firstValue);
+    expect(lasttResult).toEqual(lastValue);
+
+  })
+
 });
